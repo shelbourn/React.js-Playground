4087aea6fdc21217de117eab5cb88b5928e1004c
shelbourn <matthew@shelbourn.io>
2020-04-15 15:55:05 -0700

    Final notes.

--- notes.js
+++ notes.js
@@ -167,3 +167,18 @@ console.log(age) // Returns undefined
 
  console.log(numbers) // Returns numbers array
  console.log(doubleNumbers) // Returns double numbers array
+
+ /*****
+  * !! IMPORTANT TO KNOW !!
+  */
+
+/*  
+* map()  => https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+* find()  => https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
+* findIndex()  => https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/findIndex
+* filter()  => https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+* reduce()  => https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce?v=b
+* concat()  => https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/concat?v=b
+* slice()  => https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
+* splice()  => https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
+*/
