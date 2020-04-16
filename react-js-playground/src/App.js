b866cfb9bb1f07793985805f31f244a489a0504b
shelbourn <matthew@shelbourn.io>
2020-04-16 14:39:57 -0700

    Showing example of what React code compiles to.

--- react-js-playground/src/App.js
+++ react-js-playground/src/App.js
@@ -1,17 +1,29 @@
-// Required to import React and Component class
 import React, { Component } from 'react'
 import './App.css'
 
-// Component class imported from React library ^^
 class App extends Component {
-	// render() method is required for React to render HTML to the DOM
 	render() {
 		return (
-			// The below code is JSX (Looks like HTML, but converts to JS)
 			<div className="App">
 				<h1>Hi! I'm a React App</h1>
 			</div>
 		)
+
+		/****** 
+     * The code above ends in the same result and compiles the same,
+     * but the code below is much more cumbersome to work with
+     * 
+		// createElement('HTML Component', 'JS Obj', Children of HTML Component)
+		return React.createElement(
+			'div',
+			null,
+			// Below gets React to recognize 'h1' as an HTML element
+			// React.createElement('h1', null, 'Does this work now?')
+
+			// Below instead of 'null' we pass a JS object containing a class assignment to apply CSS styling
+			React.createElement('h1', { className: 'App' }, 'Does this work now?')
+    )
+    */
 	}
 }
 
