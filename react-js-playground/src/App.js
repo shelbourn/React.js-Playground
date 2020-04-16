dba80f8e64d4fa898ac88d52b30e19fe65753648
shelbourn <matthew@shelbourn.io>
2020-04-16 14:40:47 -0700

    Cleaning code for new lesson.

--- react-js-playground/src/App.js
+++ react-js-playground/src/App.js
@@ -8,22 +8,6 @@ class App extends Component {
 				<h1>Hi! I'm a React App</h1>
 			</div>
 		)
-
-		/****** 
-     * The code above ends in the same result and compiles the same,
-     * but the code below is much more cumbersome to work with
-     * 
-		// createElement('HTML Component', 'JS Obj', Children of HTML Component)
-		return React.createElement(
-			'div',
-			null,
-			// Below gets React to recognize 'h1' as an HTML element
-			// React.createElement('h1', null, 'Does this work now?')
-
-			// Below instead of 'null' we pass a JS object containing a class assignment to apply CSS styling
-			React.createElement('h1', { className: 'App' }, 'Does this work now?')
-    )
-    */
 	}
 }
 
