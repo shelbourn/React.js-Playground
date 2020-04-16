94296e0fcf4a2e4ff4678230292c9b03d49777cd
shelbourn <matthew@shelbourn.io>
2020-04-16 14:14:25 -0700

    Commenting React app sections

--- react-js-playground/src/App.js
+++ react-js-playground/src/App.js
@@ -1,9 +1,13 @@
+// Required to import React and Component class
 import React, { Component } from 'react'
 import './App.css'
 
+// Component class imported from React library ^^
 class App extends Component {
+	// render() method is required for React to render HTML to the DOM
 	render() {
 		return (
+			// The below code is JSX (Looks like HTML, but converts to JS)
 			<div className="App">
 				<h1>Hi! I'm a React App</h1>
 			</div>
