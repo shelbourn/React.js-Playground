17b2bda9a996131ae8d4ea4f7bd57b805ff3620f
shelbourn <matthew@shelbourn.io>
2020-04-17 15:53:45 -0700

    Children props

--- react-js-playground/src/App.js
+++ react-js-playground/src/App.js
@@ -5,6 +5,8 @@ import Person from './Person/Person'
 class App extends Component {
 	render() {
 		return (
+			// 'Hy Hobbies...' is passed between the component tags
+			// this type of input is referred to as a 'child' prop
 			<div className="App">
 				<h1>Hi! I'm a React App</h1>
 				<p>This is really working!</p>
