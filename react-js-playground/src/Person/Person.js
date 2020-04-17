a546c3c08325320df58f5247086e829e97aa4d80
shelbourn <matthew@shelbourn.io>
2020-04-17 15:14:03 -0700

    Notes about props in React

--- react-js-playground/src/Person/Person.js
+++ react-js-playground/src/Person/Person.js
@@ -2,7 +2,9 @@
 import React from 'react'
 
 // Function can have lowercase name
-const person = () => {
+// Parameter name 'props' is not necessary, but highly recommended
+// Props in React take in all property values assigned using JSX
+const person = (props) => {
 	return (
 		<p>I'm a Person and I am {Math.floor(Math.random() * 30)} years old!</p>
 	)
