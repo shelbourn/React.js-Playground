b74ca9e2d7bd867f2a926439e60eea00e9e445bd
shelbourn <matthew@shelbourn.io>
2020-04-17 15:58:38 -0700

    Children props

--- react-js-playground/src/Person/Person.js
+++ react-js-playground/src/Person/Person.js
@@ -3,9 +3,15 @@ import React from 'react'
 
 const person = (props) => {
 	return (
-		<p>
-			I'm {props.name} and I am {props.age} years old!
-		</p>
+		// {props.children} 'children' is a reserved word
+		// children are any elements (plain text included) in between
+		// the opening and closing component tags
+		<div>
+			<p>
+				I'm {props.name} and I am {props.age} years old!
+			</p>
+			<p>{props.children}</p>
+		</div>
 	)
 }
 
