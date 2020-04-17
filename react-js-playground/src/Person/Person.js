224be0a8617b6efd5ce67318086e6d88efbbf65b
shelbourn <matthew@shelbourn.io>
2020-04-17 15:48:35 -0700

    Using props

--- react-js-playground/src/Person/Person.js
+++ react-js-playground/src/Person/Person.js
@@ -1,12 +1,13 @@
 // Component file names start with capital letter (Pascal Case)
 import React from 'react'
 
-// Function can have lowercase name
 // Parameter name 'props' is not necessary, but highly recommended
 // Props in React take in all property values assigned using JSX
 const person = (props) => {
 	return (
-		<p>I'm a Person and I am {Math.floor(Math.random() * 30)} years old!</p>
+		<p>
+			I'm {props.name} and I am {props.age} years old!
+		</p>
 	)
 }
 
