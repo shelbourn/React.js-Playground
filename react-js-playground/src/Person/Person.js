0d766d0a200d03f787e78bddfd6a36f309ce2063
shelbourn <matthew@shelbourn.io>
2020-04-17 15:09:00 -0700

    Outputting dynamic content with JSX and JS

--- react-js-playground/src/Person/Person.js
+++ react-js-playground/src/Person/Person.js
@@ -3,7 +3,10 @@ import React from 'react'
 
 // Function can have lowercase name
 const person = () => {
-	return <p>I'm a Person!</p>
+	return (
+		// To render JS code within JSX it must be wrapped in {}
+		<p>I'm a Person and I am {Math.floor(Math.random() * 30)} years old!</p>
+	)
 }
 
 export default person
