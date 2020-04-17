4591b309a0bab4260ac50524859d61b23821d492
shelbourn <matthew@shelbourn.io>
2020-04-17 15:51:02 -0700

    Prepping code for 'children' props lecture

--- react-js-playground/src/Person/Person.js
+++ react-js-playground/src/Person/Person.js
@@ -1,8 +1,6 @@
 // Component file names start with capital letter (Pascal Case)
 import React from 'react'
 
-// Parameter name 'props' is not necessary, but highly recommended
-// Props in React take in all property values assigned using JSX
 const person = (props) => {
 	return (
 		<p>
