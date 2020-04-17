2c92a28386a5f6fba10a5b762cf019b76b7ac510
shelbourn <matthew@shelbourn.io>
2020-04-17 14:57:57 -0700

    Using the Person component

--- react-js-playground/src/App.js
+++ react-js-playground/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import './App.css'
 // Component must have uppercase name when importing
+// Components with lowercase names are reserved for native HTML syntax
 import Person from './Person/Person'
 
 class App extends Component {
@@ -9,6 +10,7 @@ class App extends Component {
 			<div className="App">
 				<h1>Hi! I'm a React App</h1>
 				<p>This is really working!</p>
+				<Person />
 			</div>
 		)
 	}
