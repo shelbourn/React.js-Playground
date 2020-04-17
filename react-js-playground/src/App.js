1811955a4ec7de05533219452b1402114c5ac552
shelbourn <matthew@shelbourn.io>
2020-04-17 16:03:15 -0700

    Understanding and Using State

--- react-js-playground/src/App.js
+++ react-js-playground/src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
 			<div className="App">
 				<h1>Hi! I'm a React App</h1>
 				<p>This is really working!</p>
+				<button>Switch Name</button>
 				<Person name="Matt" age="39" />
 				<Person name="Nikki" age="41">
 					My Hobbies: Reading
