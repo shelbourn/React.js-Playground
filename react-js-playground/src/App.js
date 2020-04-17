654c91490ca48a6da99199d0ff0ac8515371bd9c
shelbourn <matthew@shelbourn.io>
2020-04-17 14:23:54 -0700

    Prepping code for next lesson

--- react-js-playground/src/App.js
+++ react-js-playground/src/App.js
@@ -4,13 +4,9 @@ import './App.css'
 class App extends Component {
 	render() {
 		return (
-			// Can't use 'class' with JSX because it is a reserved JS keyword
-			//'div', 'h1', and other html-esque elements are provided by React library and not actual HTML
-			// <div> is the root element in this 'return' statement
-			// Best practice to nest everything in 1 root element
 			<div className="App">
-				{' '}
 				<h1>Hi! I'm a React App</h1>
+				<p>This is really working!</p>
 			</div>
 		)
 	}
