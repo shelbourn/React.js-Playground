7e7c71fae19fd7d6c712d9084b39b8af21ebd35d
shelbourn <matthew@shelbourn.io>
2020-04-17 18:19:40 -0700

    useState() and multiple useState() calls

--- react-js-playground/src/App.js
+++ react-js-playground/src/App.js
@@ -1,3 +1,4 @@
+// {useState} is a React 'hook' pre-defined function
 import React, { useState } from 'react'
 import './App.css'
 import Person from './Person/Person'
@@ -5,52 +6,55 @@ import Person from './Person/Person'
 // useState() always returns an array with EXACTLY 2 elements
 // The 1st element is the originally set state
 // the 2nd element is a function that allows us to update the state
+// !!!IMPORTANT!!! When using setState() with hooks, the updated state will REPLACE the original state
+// !!!IMPORTANT!!! In order to merge old state data with updated state data
+// !!!IMPORTANT!!! you have to manually add it to the function that updates the state
+// !!!IMPORTANT!!! You can bypass this problem with multiple useState() calls
 const app = (props) => {
-	const stateArr = useState({
+	// this is using array destructuring to assign the array elements
+	// of setState() to variables 'personsState' and 'setPersonsState'
+	const [personsState, setPersonsState] = useState({
 		persons: [
 			{ name: 'Matt', age: 39 },
 			{ name: 'Nikki', age: 41 },
 			{ name: 'Skylar', age: 2 },
 		],
-		otherState: 'some other value',
 	})
+
+	const [otherState, setOtherState] = useState('some other value')
+
+	console.log(personsState, otherState)
+
+	const switchNameHandler = () => {
+		setPersonsState({
+			persons: [
+				{ name: 'Matthew', age: 39 },
+				{ name: 'Nikki', age: 41 },
+				{ name: 'Skylar', age: 3 },
+			],
+		})
+	}
 
 	return (
 		<div className="App">
 			<h1>Hi! I'm a React App</h1>
 			<p>This is really working!</p>
-			<button onClick={this.switchNameHandler}>Switch Name</button>
+			<button onClick={switchNameHandler}>Switch Name</button>
 			<Person
-				name={this.state.persons[0].name}
-				age={this.state.persons[0].age}
+				name={personsState.persons[0].name}
+				age={personsState.persons[0].age}
 			/>
-			<Person name={this.state.persons[1].name} age={this.state.persons[1].age}>
+			<Person
+				name={personsState.persons[1].name}
+				age={personsState.persons[1].age}
+			>
 				My Hobbies: Reading
 			</Person>
 			<Person
-				name={this.state.persons[2].name}
-				age={this.state.persons[2].age}
+				name={personsState.persons[2].name}
+				age={personsState.persons[2].age}
 			/>
 		</div>
 	)
 }
-
 export default app
-
-// state = {
-// 	persons: [
-// 		{ name: 'Matt', age: 39 },
-// 		{ name: 'Nikki', age: 41 },
-// 		{ name: 'Skylar', age: 2 },
-// 	],
-// 	otherState: 'some other value',
-// }
-
-// switchNameHandler = () => {
-// 	this.setState({
-// 		persons: [
-// 			{ name: 'Matthew', age: 39 },
-// 			{ name: 'Nikki', age: 41 },
-// 			{ name: 'Skylar', age: 3 },
-// 		],
-// 	})
