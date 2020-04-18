fa1a4b723644ce2760b6a3f1a44ee0ce62e5f9e7
shelbourn <matthew@shelbourn.io>
2020-04-18 01:26:44 -0700

    Cleaning code for submission.

--- base-syntax--assignment-problem/src/UserOutput/UserOutput.js
+++ base-syntax--assignment-problem/src/UserOutput/UserOutput.js
@@ -5,8 +5,8 @@ const userOutput = (props) => {
 	return (
 		<div className="UserOutput">
 			<p>
-				Hello world! My name is {props.userName} {props.userLName} and this is
-				one of my first attempts at building a React app.
+				Hello world! My name is {props.userName} and this is one of my first
+				attempts at building a React app.
 			</p>
 			<p>
 				I'm super excited about this and can't wait to see what I will learn
