52425b2c40ca8de144648acbbd9e22da94a821a7
shelbourn <matthew@shelbourn.io>
2020-04-17 17:38:32 -0700

    Using hooks instead of state with class-based comp

--- react-js-playground/src/App.js
+++ react-js-playground/src/App.js
@@ -3,49 +3,43 @@ import './App.css'
 import Person from './Person/Person'
 
 // App is a class-based component
-class App extends Component {
-	state = {
-		persons: [
-			{ name: 'Matt', age: 39 },
-			{ name: 'Nikki', age: 41 },
-			{ name: 'Skylar', age: 2 },
-		],
-		otherState: 'some other value',
-	}
-
-	switchNameHandler = () => {
-		this.setState({
-			persons: [
-				{ name: 'Matthew', age: 39 },
-				{ name: 'Nikki', age: 41 },
-				{ name: 'Skylar', age: 3 },
-			],
-		})
-	}
-
-	render() {
-		return (
-			<div className="App">
-				<h1>Hi! I'm a React App</h1>
-				<p>This is really working!</p>
-				<button onClick={this.switchNameHandler}>Switch Name</button>
-				<Person
-					name={this.state.persons[0].name}
-					age={this.state.persons[0].age}
-				/>
-				<Person
-					name={this.state.persons[1].name}
-					age={this.state.persons[1].age}
-				>
-					My Hobbies: Reading
-				</Person>
-				<Person
-					name={this.state.persons[2].name}
-					age={this.state.persons[2].age}
-				/>
-			</div>
-		)
-	}
+const app = (props) => {
+	return (
+		<div className="App">
+			<h1>Hi! I'm a React App</h1>
+			<p>This is really working!</p>
+			<button onClick={this.switchNameHandler}>Switch Name</button>
+			<Person
+				name={this.state.persons[0].name}
+				age={this.state.persons[0].age}
+			/>
+			<Person name={this.state.persons[1].name} age={this.state.persons[1].age}>
+				My Hobbies: Reading
+			</Person>
+			<Person
+				name={this.state.persons[2].name}
+				age={this.state.persons[2].age}
+			/>
+		</div>
+	)
 }
 
-export default App
+export default app
+
+// state = {
+// 	persons: [
+// 		{ name: 'Matt', age: 39 },
+// 		{ name: 'Nikki', age: 41 },
+// 		{ name: 'Skylar', age: 2 },
+// 	],
+// 	otherState: 'some other value',
+// }
+
+// switchNameHandler = () => {
+// 	this.setState({
+// 		persons: [
+// 			{ name: 'Matthew', age: 39 },
+// 			{ name: 'Nikki', age: 41 },
+// 			{ name: 'Skylar', age: 3 },
+// 		],
+// 	})
