54716fa8cf370ff7da976661dcf873842086ee45
shelbourn <matthew@shelbourn.io>
2020-04-18 15:49:19 -0700

    Using .map() method to map array in state to JSX

--- react-js-playground/src/App.js
+++ react-js-playground/src/App.js
@@ -50,25 +50,13 @@ class App extends Component {
 		}
 
 		let persons = null
+
 		if (this.state.showPersons) {
 			persons = (
 				<div>
-					<Person
-						name={this.state.persons[0].name}
-						age={this.state.persons[0].age}
-					/>
-					<Person
-						name={this.state.persons[1].name}
-						age={this.state.persons[1].age}
-						click={this.switchNameHandler.bind(this, 'Matt!')}
-						changed={this.nameChangedHandler}
-					>
-						My Hobbies: Reading
-					</Person>
-					<Person
-						name={this.state.persons[2].name}
-						age={this.state.persons[2].age}
-					/>
+					{this.state.persons.map((person) => {
+						return <Person name={person.name} age={person.age} />
+					})}
 				</div>
 			)
 		}
