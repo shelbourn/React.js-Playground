681b2af57123c079a83cc3bb86e4a198b4372bbc
shelbourn <matthew@shelbourn.io>
2020-04-17 17:12:00 -0700

    Event handling with methods

--- react-js-playground/src/App.js
+++ react-js-playground/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import './App.css'
 import Person from './Person/Person'
 
+// ! 'this.property' syntax refers to property in parent class
 class App extends Component {
 	state = {
 		persons: [
@@ -11,12 +12,19 @@ class App extends Component {
 		],
 	}
 
+	// When referencing this handler do not inlcude ()
+	// Because then the function would be executed as soon as the
+	// code was rendered by the DOM
+	switchNameHandler = () => {
+		console.log('Was clicked!')
+	}
+
 	render() {
 		return (
 			<div className="App">
 				<h1>Hi! I'm a React App</h1>
 				<p>This is really working!</p>
-				<button>Switch Name</button>
+				<button onClick={this.switchNameHandler}>Switch Name</button>
 				<Person
 					name={this.state.persons[0].name}
 					age={this.state.persons[0].age}
