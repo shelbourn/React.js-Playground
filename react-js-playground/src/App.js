52614246ba20771cc737634f9807013839640b1b
shelbourn <matthew@shelbourn.io>
2020-04-18 15:20:48 -0700

    Using JS to handle conditional output

--- react-js-playground/src/App.js
+++ react-js-playground/src/App.js
@@ -49,6 +49,30 @@ class App extends Component {
 			borderRadius: '10px',
 		}
 
+		let persons = null
+		if (this.state.showPersons) {
+			persons = (
+				<div>
+					<Person
+						name={this.state.persons[0].name}
+						age={this.state.persons[0].age}
+					/>
+					<Person
+						name={this.state.persons[1].name}
+						age={this.state.persons[1].age}
+						click={this.switchNameHandler.bind(this, 'Matt!')}
+						changed={this.nameChangedHandler}
+					>
+						My Hobbies: Reading
+					</Person>
+					<Person
+						name={this.state.persons[2].name}
+						age={this.state.persons[2].age}
+					/>
+				</div>
+			)
+		}
+
 		return (
 			<div className="App">
 				<h1>Hi, I'm a React App</h1>
@@ -56,26 +80,7 @@ class App extends Component {
 				<button style={style} onClick={this.togglePersonsHandler}>
 					Show/Hide Names
 				</button>
-				{this.state.showPersons ? (
-					<div>
-						<Person
-							name={this.state.persons[0].name}
-							age={this.state.persons[0].age}
-						/>
-						<Person
-							name={this.state.persons[1].name}
-							age={this.state.persons[1].age}
-							click={this.switchNameHandler.bind(this, 'Matt!')}
-							changed={this.nameChangedHandler}
-						>
-							My Hobbies: Reading
-						</Person>
-						<Person
-							name={this.state.persons[2].name}
-							age={this.state.persons[2].age}
-						/>
-					</div>
-				) : null}
+				{persons}
 			</div>
 		)
 	}
