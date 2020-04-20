c6807009bb2eb1913ed407177113e7647abadd94
shelbourn <matthew@shelbourn.io>
2020-04-20 16:08:01 -0700

    Commenting code.

--- react-js-playground/src/ErrorBoundary/ErrorBoundary.js
+++ react-js-playground/src/ErrorBoundary/ErrorBoundary.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 
 //! Only wrap elements with <ErrorBoundary> when you think that code
-//! May fail. DON'! WRAP ALL CODE IN <ErrorBoundary>
+//! May fail and you can't control it. DON'! WRAP ALL CODE IN <ErrorBoundary>
 class ErrorBoundary extends Component {
 	state = {
 		hasError: false,
@@ -16,6 +16,8 @@ class ErrorBoundary extends Component {
 		if (this.state.hasError) {
 			return <h1>{this.state.errorMessage}</h1>
 		} else {
+			// returning props.children returns all the normal code within the <div>
+			// because all other elements are 'children' of the <ErrorBoundary> element
 			return this.props.children
 		}
 	}
