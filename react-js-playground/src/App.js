9aa9c9e94fdda1340efe6a7b38a5f19b9e712d0d
shelbourn <matthew@shelbourn.io>
2020-04-19 20:18:29 -0700

    CSS modules

--- react-js-playground/src/App.js
+++ react-js-playground/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-import './App.css'
+import classes from './App.css'
 import Person from './Person/Person'
 
 class App extends Component {
@@ -58,6 +58,8 @@ class App extends Component {
 
 		let persons = null
 
+		let btnClass = ''
+
 		// Code to manage the 'Persons' array (list)
 		if (this.state.showPersons) {
 			persons = (
@@ -77,26 +79,24 @@ class App extends Component {
 			)
 			// Dynamically styling output if 'showPersons' is true
 			buttonText = 'Hide Names'
+			btnClass = classes.Red
 		}
 
-		const classes = []
+		const assignedClasses = []
 		if (this.state.persons.length <= 2) {
-			classes.push('red') // classes = ['red']
+			assignedClasses.push(classes.red) // classes = ['red']
 		}
 		if (this.state.persons.length <= 1) {
-			classes.push('bold') // classes = ['red', 'bold']
+			assignedClasses.push(classes.bold) // classes = ['red', 'bold']
 		}
 
 		return (
-			<div className="App">
+			<div className={classes.App}>
 				<h1>Hi, I'm a React App</h1>
-				<p className={classes.join(' ')}>This is really working!</p>
-				<StyledButton
-					alt={this.state.showPersons}
-					onClick={this.togglePersonsHandler}
-				>
+				<p className={assignedClasses.join(' ')}>This is really working!</p>
+				<button className={btnClass} onClick={this.togglePersonsHandler}>
 					{buttonText}
-				</StyledButton>
+				</button>
 				{persons}
 			</div>
 		)
