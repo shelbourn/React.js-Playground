de20df3a7af291d789e69e0171612b9fe192b0ff
shelbourn <matthew@shelbourn.io>
2020-04-20 16:10:22 -0700

    Cleaning code for next module

--- react-js-playground/src/App.js
+++ react-js-playground/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import classes from './App.css'
 import Person from './Person/Person'
-import ErrorBoundary from './ErrorBoundary/ErrorBoundary'
 
 class App extends Component {
 	state = {
@@ -67,14 +66,13 @@ class App extends Component {
 					{this.state.persons.map((person, index) => {
 						return (
 							// Key must always be on the outer wrapper of an element using the .map() method
-							<ErrorBoundary key={person.id}>
-								<Person
-									click={() => this.deletePersonHandler(index)}
-									name={person.name}
-									age={person.age}
-									changed={(event) => this.nameChangedHandler(event, person.id)}
-								/>
-							</ErrorBoundary>
+							<Person
+								key={person.id}
+								click={() => this.deletePersonHandler(index)}
+								name={person.name}
+								age={person.age}
+								changed={(event) => this.nameChangedHandler(event, person.id)}
+							/>
 						)
 					})}
 				</div>
