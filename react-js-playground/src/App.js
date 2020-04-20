c14f5ea09e23cdfecf00a7ab2e7a9e203723352e
shelbourn <matthew@shelbourn.io>
2020-04-20 16:01:54 -0700

    ErrorBoundary in use

--- react-js-playground/src/App.js
+++ react-js-playground/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
-
 import classes from './App.css'
 import Person from './Person/Person'
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary'
 
 class App extends Component {
 	state = {
@@ -66,13 +66,15 @@ class App extends Component {
 				<div>
 					{this.state.persons.map((person, index) => {
 						return (
-							<Person
-								click={() => this.deletePersonHandler(index)}
-								name={person.name}
-								age={person.age}
-								key={person.id}
-								changed={(event) => this.nameChangedHandler(event, person.id)}
-							/>
+							// Key must always be on the outer wrapper of an element
+							<ErrorBoundary key={person.id}>
+								<Person
+									click={() => this.deletePersonHandler(index)}
+									name={person.name}
+									age={person.age}
+									changed={(event) => this.nameChangedHandler(event, person.id)}
+								/>
+							</ErrorBoundary>
 						)
 					})}
 				</div>
