e315c8375758924cbb5968722668ac9e856e6114
shelbourn <matthew@shelbourn.io>
2020-04-21 13:56:29 -0700

    Restructuring file tree & adding folders

--- react-js-playground/src/App.js
+++ react-js-playground/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import classes from './App.css'
-import Person from './Persons/Person/Person'
+import Person from './components/Persons/Person/Person'
 
 class App extends Component {
 	//! Logic and global variables go in the main class body
