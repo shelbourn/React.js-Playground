ec268e585cc783d78ce7443ed5c3d5bf8a8da61c
shelbourn <matthew@shelbourn.io>
2020-04-22 17:23:08 -0700

    Adding updating lifecycle components

--- react-js-playground/src/components/Persons/Persons.js
+++ react-js-playground/src/components/Persons/Persons.js
@@ -2,6 +2,26 @@ import React, { Component } from 'react'
 import Person from './Person/Person'
 
 class Persons extends Component {
+	// static getDerivedStateFromProps(props, state) {
+	// 	console.log('[Persons.js] getDerivedStateFromProps')
+	// 	return state
+	// }
+
+	shouldComponentUpdate(nextProps, nextState) {
+		console.log('[Persons.js] shouldComponentUpdate')
+		return true
+	}
+
+	getSnapshotBeforeUpdate(prevProps, prevState) {
+		console.log('[Persons.js] getSnapshotBeforeUpdate')
+		return { message: 'Snapshot!' }
+	}
+
+	componentDidUpdate(prevProps, prevState, snapshot) {
+		console.log('[Persons.js] componentDidUpdate')
+		console.log(snapshot)
+	}
+
 	render() {
 		console.log('[Persons.js] rendering...')
 		return this.props.persons.map((person, index) => {
