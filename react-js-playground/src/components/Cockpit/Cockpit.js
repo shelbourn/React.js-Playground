cc7bcac35e7dbfcf81a1fe4db2892b54acccf6e0
shelbourn <matthew@shelbourn.io>
2020-04-22 23:43:22 -0700

    Targetting {useEffect}

--- react-js-playground/src/components/Cockpit/Cockpit.js
+++ react-js-playground/src/components/Cockpit/Cockpit.js
@@ -3,12 +3,22 @@ import classes from './Cockpit.css'
 // {useEffect} hook is the second most important component in React
 // after {useState} hook
 // {useEffect} is a React hook and NOT a lifecycle hookb
+// {useEffect} = componentDidMount & componentDidUpdate combined into one
 
 const cockpit = (props) => {
 	// Must pass a function into {useEffect}
 	useEffect(() => {
 		console.log('[Cockpit.js] useEffect')
-	})
+		// http request...
+		setTimeout(() => {
+			alert('Saved data to the cloud!')
+		}, 1000)
+	}, [props.persons]) // Only triggers on initial page load & when 'persons' updates
+	// arg2 = [] empty bracket to trigger only on initial page load and never again
+	// 2nd argument must be contained in square brackets []
+
+	// Can use {useEffect() as many times as necessary to manage different data}
+	// useEffect(() => {...}, [arg2 = data to be targeted by useEffect])
 
 	const assignedClasses = []
 	let btnClass = ''
