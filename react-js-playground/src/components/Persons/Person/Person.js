9c071a23bf62ba77a26fcd276feb5efead3661cd
shelbourn <matthew@shelbourn.io>
2020-04-25 12:23:02 -0700

    Passing unknown props into a HOC w/spread op

--- react-js-playground/src/components/Persons/Person/Person.js
+++ react-js-playground/src/components/Persons/Person/Person.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import classes from './Person.css'
+import Auxiliary from '../../../hoc/Auxiliary'
+import withClass from '../../../hoc/withClass'
 
 class Person extends Component {
 	render() {
 		console.log('[Person.js] rendering...')
 		return (
-			<div className={classes.Person}>
+			<Auxiliary>
 				<p onClick={this.props.click}>
 					I'm {this.props.name} and I am {this.props.age} years old!
 				</p>
@@ -15,7 +17,7 @@ class Person extends Component {
 					onChange={this.props.changed}
 					value={this.props.name}
 				/>
-			</div>
+			</Auxiliary>
 		)
 	}
 }
