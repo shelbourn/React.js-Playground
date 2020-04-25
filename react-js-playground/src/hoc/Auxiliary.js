71e933753e234c2ecd15d394d18fbd9cfd8100b5
shelbourn <matthew@shelbourn.io>
2020-04-24 23:10:02 -0700

    HOC and JSX adjacent components

--- react-js-playground/src/hoc/Auxiliary.js
+++ react-js-playground/src/hoc/Auxiliary.js
@@ -1,6 +1,13 @@
 import React from 'react'
 
 //? HOC (higher order components) wrap other components
+//? Aux component is a HOC that only returns the children props within it's scope
+//? The Aux component itself isn't doing anything besides providing a
+//? way to multiple adjacent JSX elements in React
+//? This has to be this way because Javascript can only return one expression
+
+//? props.children is baked into React and returns all JSX that is a child
+//? to the main wrapping element
 
 const aux = (props) => props.children
 
