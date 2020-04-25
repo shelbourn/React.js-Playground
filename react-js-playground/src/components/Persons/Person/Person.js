7e0296d34857d6cbd05ab6c57a59573ce9a169e5
shelbourn <matthew@shelbourn.io>
2020-04-24 23:05:18 -0700

    Updating .eslintrc.json

--- react-js-playground/src/components/Persons/Person/Person.js
+++ react-js-playground/src/components/Persons/Person/Person.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import classes from './Person.css'
+import Aux from '../../../hoc/Auxiliary'
 
 //? To return adjacent JSX elements you can eliminate the wrapping <div></div>
 //? and wrap the block in [] and separate the JSX elements
@@ -12,7 +13,8 @@ class Person extends Component {
 	render() {
 		console.log('[Person.js] rendering...')
 		return (
-			<div className={classes.Person}>
+			<Aux>
+				{/* // <div className={classes.Person}> */}
 				<p onClick={this.props.click}>
 					I'm {this.props.name} and I am {this.props.age} years old!
 				</p>
@@ -22,7 +24,8 @@ class Person extends Component {
 					onChange={this.props.changed}
 					value={this.props.name}
 				/>
-			</div>
+				{/* // </div> */}
+			</Aux>
 		)
 	}
 }
---
+++ react-js-playground/src/hoc/Auxiliary.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+//? HOC (higher order components) wrap other components
+
+const aux = (props) => props.children
+
+export default aux
