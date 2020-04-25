563d4f5701e802b4e2b20872d093115b5c5ca581
shelbourn <matthew@shelbourn.io>
2020-04-25 11:00:51 -0700

    Updating file tree.

--- react-js-playground/src/components/Persons/Person/Person.js
+++ react-js-playground/src/components/Persons/Person/Person.js
@@ -1,13 +1,12 @@
 import React, { Component, Fragment } from 'react'
 import classes from './Person.css'
-import Aux from '../../../hoc/Auxiliary/Auxiliary'
+import Aux from '../../../hoc/Auxiliary'
 
 class Person extends Component {
 	render() {
 		console.log('[Person.js] rendering...')
 		return (
 			<Aux>
-				{/* // <div className={classes.Person}> */}
 				<p onClick={this.props.click}>
 					I'm {this.props.name} and I am {this.props.age} years old!
 				</p>
@@ -17,7 +16,6 @@ class Person extends Component {
 					onChange={this.props.changed}
 					value={this.props.name}
 				/>
-				{/* // </div> */}
 			</Aux>
 		)
 	}
