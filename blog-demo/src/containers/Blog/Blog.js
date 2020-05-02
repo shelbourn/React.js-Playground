17c77925513a6aef25d23dc6444c15dc384481da
shelbourn <matthew@shelbourn.io>
2020-05-01 23:55:24 -0700

    Commenting code and adding axios get.

--- blog-demo/src/containers/Blog/Blog.js
+++ blog-demo/src/containers/Blog/Blog.js
@@ -1,28 +1,38 @@
-import React, { Component } from 'react';
-
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
-import './Blog.css';
+import React, { Component } from 'react'
+import axios from 'axios'
+import Post from '../../components/Post/Post'
+import FullPost from '../../components/FullPost/FullPost'
+import NewPost from '../../components/NewPost/NewPost'
+import './Blog.css'
 
 class Blog extends Component {
-    render () {
-        return (
-            <div>
-                <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
-                </section>
-                <section>
-                    <FullPost />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
-        );
-    }
+	//% Axios is a package for easily managing http/ajax requests
+	//* componentDidMount() is a great place for attaching an http request
+	//* because it can be executed as a side-effect without resulting
+	//* in a React re-render before the request has completed.
+	//? axios has PROMISE objects built-in. Therefore, you can chain .then onto it
+
+	componentDidMount() {
+		axios.get('https://jsonplaceholder.typicode.com/posts')
+	}
+
+	render() {
+		return (
+			<div>
+				<section className="Posts">
+					<Post />
+					<Post />
+					<Post />
+				</section>
+				<section>
+					<FullPost />
+				</section>
+				<section>
+					<NewPost />
+				</section>
+			</div>
+		)
+	}
 }
 
-export default Blog;
+export default Blog
