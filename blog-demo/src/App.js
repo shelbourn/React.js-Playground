8f2f791124087c9584e4d6477becbea643df829f
shelbourn <matthew@shelbourn.io>
2020-05-04 12:29:00 -0700

    Routing demo with notes.

--- blog-demo/src/App.js
+++ blog-demo/src/App.js
@@ -4,6 +4,7 @@ import Blog from './containers/Blog/Blog'
 
 //! For React Router, you must wrap the component you want to
 //! enable routes in, in either App.js (root js file) or index.js
+//! Wrap this component with the imported {BrowserRouter} component
 
 //* With React Router enabled, it makes sense to move routed components
 //* into the Containers folder because they are more like containers now
--- blog-demo/src/containers/Blog/Blog.js
+++ blog-demo/src/containers/Blog/Blog.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 // import axios from 'axios'
 import './Blog.css'
 import Posts from './Posts/Posts'
+import { Route } from 'react-router-dom'
 
 //% Axios is a package for easily managing http/ajax requests
 //* componentDidMount() is a great place for attaching an http request
@@ -10,6 +11,9 @@ import Posts from './Posts/Posts'
 //? axios has PROMISE objects built-in. Therefore, you can chain .then onto it
 
 //! Handling errors locally within components is a best practice and makes sense
+
+//* To enable Routing for an individual container that you want to route to
+//* you use the imported {Route} component
 
 class Blog extends Component {
 	render() {
@@ -27,7 +31,19 @@ class Blog extends Component {
 						</ul>
 					</nav>
 				</header>
-        <Posts/>
+				{/* 'path', 'exact', 'render', etc are reserved props/keywords for React Router
+         exact holds a bool of true (default is false). If true (exact) then whatever is
+         designated by 'path' needs to be exact for the 'render' designation to show.
+         Otherwise, if 'exact' is omitted (false) then the path designation will act like a 
+         prefix and whatever paths include the 'path' designation will render the value stored
+         in 'render' */}
+				{/* You can have as many routes as you'd like for the same path
+         (including nested and sub-routes. These other routes may render different elements to 
+         the same path.) */}
+				<Route path="/" exact render={() => <h1>Home</h1>} />
+				{/* Here 'Home2' will be the only element displayed on the '/new-post' page because 'exact' is omitted */}
+				<Route path="/" render={() => <h1>Home 2</h1>} />
+				<Route path="/" exact render={() => <h1>Home 3</h1>} />
 			</div>
 		)
 	}
