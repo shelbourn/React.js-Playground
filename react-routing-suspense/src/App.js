83819f5b455736c32ac38987a41d5b32ff61bb1c
shelbourn <matthew@shelbourn.io>
2020-05-06 15:44:45 -0700

    lazy-loading/code-splitting with React.lazy() & Suspense

--- react-routing-suspense/src/App.js
+++ react-routing-suspense/src/App.js
@@ -1,26 +1,40 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route, NavLink } from 'react-router-dom';
+import React, { Component, Suspense } from 'react'
+import { BrowserRouter, Route, NavLink } from 'react-router-dom'
 
-import Posts from './containers/Posts';
-import User from './containers/User';
-import Welcome from './containers/Welcome';
+import User from './containers/User'
+import Welcome from './containers/Welcome'
+
+//* Named exports are not supported in React.lazy()
+const Posts = React.lazy(() => import('./containers/Posts'))
 
 class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <React.Fragment>
-          <nav>
-            <NavLink to="/user">User Page</NavLink> |&nbsp;
-            <NavLink to="/posts">Posts Page</NavLink>
-          </nav>
-          <Route path="/" component={Welcome} exact />
-          <Route path="/user" component={User} />
-          <Route path="/posts" component={Posts} />
-        </React.Fragment>
-      </BrowserRouter>
-    );
-  }
+	render() {
+		return (
+			<BrowserRouter>
+				{/* React.Fragment is the equivalent to our Aux component but
+      it is now built into React */}
+				<React.Fragment>
+					<nav>
+						<NavLink to="/user">User Page</NavLink> |&nbsp;
+						<NavLink to="/posts">Posts Page</NavLink>
+					</nav>
+					<Route path="/" component={Welcome} exact />
+					<Route path="/user" component={User} />
+					<Route
+						path="/posts"
+						render={() => (
+							//* Suspense wraps the lazy-loaded component (const in our case)
+							//* fallback prop is something that shows while the component is being loaded
+							//* fallback is convenient for displaying spinners, etc
+							<Suspense fallback={<div>Loading...</div>}>
+								<Posts />
+							</Suspense>
+						)}
+					/>
+				</React.Fragment>
+			</BrowserRouter>
+		)
+	}
 }
 
-export default App;
+export default App
