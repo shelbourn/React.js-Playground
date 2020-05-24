7acb5c93996f84eb615cd5893c4ac84c9747f68b
shelbourn <matthew@shelbourn.io>
2020-05-24 14:29:40 -0700

    Enabling Redux in React project with Provider

--- redux--01-start/src/index.js
+++ redux--01-start/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
@@ -9,5 +10,11 @@ import reduxReducer from '../store/reducer'
 //* Creating Redux store and implementing reducer
 const reduxStore = createStore(reduxReducer)
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+	//* Provider connects Redux to React and the store prop enable the Redux store
+	<Provider store={reduxStore}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+)
 registerServiceWorker()
