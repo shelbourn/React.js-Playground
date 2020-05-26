25a5d808bcbb425ed1ccdd47da8eea672b80cd9f
shelbourn <matthew@shelbourn.io>
2020-05-26 15:28:55 -0700

    Commenting code

--- redux--01-start/src/store/reducer.js
+++ redux--01-start/src/store/reducer.js
@@ -7,40 +7,34 @@ const initialState = {
  * * such as action.payload.value (see Counter.js)
  */
 
+// No need for the break statement in this switch/case because
+// the return statement automatically exits and returns a value
 const reduxReducer = (state = initialState, action) => {
 	switch (action.type) {
-		case 'INCREMENT':
-			{
-				return {
-					...state,
-					counter: state.counter + 1,
-				}
+		case 'INCREMENT': {
+			return {
+				...state,
+				counter: state.counter + 1,
 			}
-			break
-		case 'DECREMENT':
-			{
-				return {
-					...state,
-					counter: state.counter - 1,
-				}
+		}
+		case 'DECREMENT': {
+			return {
+				...state,
+				counter: state.counter - 1,
 			}
-			break
-		case 'ADD':
-			{
-				return {
-					...state,
-					counter: state.counter + action.payload.value,
-				}
+		}
+		case 'ADD': {
+			return {
+				...state,
+				counter: state.counter + action.payload.value,
 			}
-			break
-		case 'SUBTRACT':
-			{
-				return {
-					...state,
-					counter: state.counter - action.payload.value,
-				}
+		}
+		case 'SUBTRACT': {
+			return {
+				...state,
+				counter: state.counter - action.payload.value,
 			}
-			break
+		}
 	}
 	return state
 }
