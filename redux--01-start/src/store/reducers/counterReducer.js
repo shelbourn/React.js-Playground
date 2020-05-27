60634dcce027d6fe965607582b5d3de31692f25d
shelbourn <matthew@shelbourn.io>
2020-05-27 14:49:53 -0700

    Refactoring code to include 2 reducers

---
+++ redux--01-start/src/store/reducers/counterReducer.js
@@ -0,0 +1,87 @@
+import * as actionTypes from '../actions'
+
+const initialState = {
+	counter: 0,
+}
+
+/***
+ * * Can pass additional object properties along with the action
+ * * such as action.payload.value (see Counter.js)
+ */
+
+/***
+ * ! Always use .concat() to update arrays in state immutably and NOT .push()
+ * ! .push() will update the original state, even if the state has already been copied
+ * ! .concat() will add elements to a new copy of the array in state
+ */
+
+// No need for the break statement in this switch/case because
+// the return statement automatically exits and returns a value
+const reduxReducer = (state = initialState, action) => {
+	switch (action.type) {
+		case actionTypes.INCREMENT: {
+			// This is one way to copy state and update it immutably
+			// NOT A DEEP CLONE
+			const newState = Object.assign({}, state)
+			newState.counter = state.counter + 1
+			return newState
+		}
+		case actionTypes.DECREMENT: {
+			// This is another (shorter) way to copy & update state immutably
+			// NOT A DEEP CLONE
+			return {
+				...state,
+				counter: state.counter - 1,
+			}
+		}
+		case actionTypes.ADD: {
+			return {
+				...state,
+				counter: state.counter + action.payload.value,
+			}
+		}
+		case actionTypes.SUBTRACT: {
+			return {
+				...state,
+				counter: state.counter - action.payload.value,
+			}
+		}
+		case actionTypes.STORE_RESULT: {
+			return {
+				...state,
+				//! Use .concat() here because .push() does not update
+				//! the state immutably, it will still modify the original state
+				//! .concat() adds an array element to a copied array
+				results: state.results.concat({ id: new Date(), value: state.counter }),
+			}
+		}
+		case actionTypes.DELETE_RESULT: {
+			//? Method one for copying/updating an array in state
+			//? Below will not create a deep clone so if the array contains objects
+			//? then those objects will have to be copied too
+			//? The .splice() method MUST be performed on a copy of the array in state
+			//? because .splice() will act on the original array if not
+			// const id = 2
+			// const newArray = [...state.results]
+			// newArray.splice(id, 1)
+
+			//? Method two for copying/updating an array in state
+			//? .filter() creates a new array whose elements meet the criteria
+			//? of the .filter() function
+			//? In this filter we are returning true if the element's ID does not match
+			//? the ID of the element we are receiving an action from (element clicked on to delete it)
+			const updatedArray = state.results.filter(
+				(result) => result.id !== action.payload.resultElementID
+			)
+			return {
+				...state,
+				results: updatedArray,
+			}
+		}
+	}
+	//? This return statement returns the initial or current state if
+	//? no case is matched. If this return is absent, then the app will break
+	return state
+}
+
+export default reduxReducer
