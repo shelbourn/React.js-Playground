f809b024ae865178e1acfd1907e5f9c634c24def
shelbourn <matthew@shelbourn.io>
2020-05-28 15:01:15 -0700

    Syncing updates

--- redux--assignment-2-problem/src/components/AddPerson/AddPerson.js
+++ redux--assignment-2-problem/src/components/AddPerson/AddPerson.js
@@ -1,18 +1,46 @@
-import React {Component} from 'react'
+import React, { Component } from 'react'
 
 import './AddPerson.css'
 import { Component } from 'react'
 
 class AddPerson extends Component {
-  render() {
-    return (
-	<div className="AddPerson">
-		<input type="text" placeholder="Name" />
-		<input type="number" placeholder="Age" />
-		<button onClick={this.props.personAdded}>Add Person</button>
-	</div>
-    )
-  }
+	state = {
+		name: '',
+		age: null,
+	}
+
+	nameChangedHandler = (event) => {
+		this.setState({ name: event.target.value })
+	}
+
+	ageChangedHandler = (event) => {
+		this.setState({ age: event.target.vale })
+	}
+
+	/***
+	 * ? Two-way binding is set up by having an onChange handler that handles the
+	 * ? input event and updates the state, and then the value set to the equivalent state property
+	 */
+
+	render() {
+		return (
+			<div className="AddPerson">
+				<input
+					type="text"
+					placeholder="Name"
+					onChange={this.nameChangedHandler}
+					value={this.state.name}
+				/>
+				<input
+					type="number"
+					placeholder="Age"
+					onChange={this.ageChangedHandler}
+					value={this.state.age}
+				/>
+				<button onClick={this.props.personAdded}>Add Person</button>
+			</div>
+		)
+	}
 }
 
-export default addPerson
+export default AddPerson
